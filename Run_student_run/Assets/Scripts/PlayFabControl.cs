8c8509cfaddd6ff7b72c60c1919eac278b361c37
LatiniDav <91899784+LatiniDav@users.noreply.github.com>
2022-03-23 13:06:23 +0100

    Add encryption e placeolder

--- Run_student_run/Assets/Scripts/PlayFabControl.cs
+++ Run_student_run/Assets/Scripts/PlayFabControl.cs
@@ -23,5 +23,21 @@ public class PlayFabControl : MonoBehaviour
         logInTab.SetActive(true);
     }
 
-    
+    string Encrypt(string pass)
+    {
+
+        System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+        byte[] bs = System.TextEncoding.UTF8.GetBytes(pass);
+        bs = x.ComputeHash(bs);
+        System.Text.StringBuilder s = new System.Text.StringBuilder();
+        foreach(byte b in bs)
+        {
+            s.Append(b.ToString("x2").ToLower());
+        }
+        return s.ToString();
+    }
+
+    //{
+        }
+
 }
